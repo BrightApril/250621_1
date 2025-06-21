5ff2d16ee5d676ac68ea619694dda203cfa79896
BrightApril <156571683+BrightApril@users.noreply.github.com>
2025-06-21 13:10:01 +0900

    Update 00_emoji.py

--- pages/00_emoji.py
+++ pages/00_emoji.py
@@ -1,1 +1,2 @@
-
+import streamlit as st
+st.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmQxamw2cWt5Zm54anV3Z2pzZXNvenh3dGVxNDRiMjJrOTc0cjM3ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l1KVaj5UcbHwrBMqI/giphy.gif")
